8e3a7aadb8bf43bb5d5e367050e0ee6728c9b54e
Bartosz Górka <bartosz.gorka@appunite.com>
2018-05-19 13:11:44 +0200

    Tags

---
+++ tags.h
@@ -0,0 +1,33 @@
+/*
+ * Thieves' Guild project
+ *  Poznań, May 2018
+ *
+ * tags.h
+ *  MPI Tags header file, all tags prepared in single file to enable easier
+ *  modify this values and eliminate duplicate code.
+ *
+ * Authors:
+ *  Jarosław Skrzypczak index. 127265 (@jarkendar)
+ *  Bartosz Górka index. 127228 (@bartoszgorka)
+ */
+
+#ifndef TAG_FIND_PARTNER
+/*
+ * Tag - start find partner process
+ */
+#define TAG_FIND_PARTNER 110
+#endif
+
+#ifndef TAG_ALREADY_PAIRED
+/*
+ * Tag - already in pair
+ */
+#define TAG_ALREADY_PAIRED 120
+#endif
+
+#ifndef TAG_ACCEPT_PARTNER
+/*
+ * Tag - acceptance of the proposal
+ */
+#define TAG_ACCEPT_PARTNER 130
+#endif
